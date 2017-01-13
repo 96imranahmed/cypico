c38210b6f704e5028e605e7cc0f02c7e6cceff47
Imran Ahmed <96imranahmed@gmail.com>
2017-01-13 02:30:43 +0000

    Fixed headers

--- cypico/pico_wrapper.cpp
+++ cypico/pico_wrapper.cpp
@@ -1,14 +1,22 @@
 #include "pico_wrapper.h"
 
 
-int pico_detect_objects(const unsigned char* image, const int height,
-                        const int width, const int width_step,
-                        const unsigned char* cascades, const int max_detections,
-                        const int n_orientations, const float* orientations,
+int pico_detect_objects(const unsigned char* image, 
+                        const int height,
+                        const int width,
+                        const int width_step,
+                        const unsigned char* cascades, 
+                        const int max_detections,
+                        const int n_orientations, 
+                        const float* orientations,
                         const float scale_factor,
-                        const float stride_factor, const float min_size,
+                        const float stride_factor, 
+                        const float min_size,
                         const float q_cutoff,
-                        float* qs, float* rs, float* cs, float* ss,
+                        float* qs, 
+                        float* rs, 
+                        float* cs, 
+                        float* ss,
                         float* os) {
     int n_detections = 0;
     int n_new_detections = 0;
--- cypico/pico_wrapper.h
+++ cypico/pico_wrapper.h
@@ -13,11 +13,21 @@ static const unsigned char FACE_CASCADES[] =
 static const long FACE_CASCADES_SIZE = (long) (sizeof(FACE_CASCADES) / sizeof(unsigned char));
 
 
-int pico_detect_objects(const unsigned char* image, const int height,
-                        const int width, const int width_step,
-                        const unsigned char* cascades, const int max_detections,
-                        const int n_orientations, const float* orientations,
-                        const float scale_factor, const float stride_factor,
-                        const float min_size, const float q_cutoff,
-                        float* qs, float* rs, float* cs, float* ss, float* os);
+int pico_detect_objects(const unsigned char* image, 
+                        const int height,
+                        const int width, 
+                        const int width_step,
+                        const unsigned char* cascades, 
+                        const int max_detections,
+                        const int n_orientations, 
+                        const float* orientations,
+                        const float scale_factor, 
+                        const float stride_factor,
+                        const float min_size, 
+                        const float q_cutoff,
+                        float* qs, 
+                        float* rs, 
+                        float* cs, 
+                        float* ss,
+                        float* os);
 
